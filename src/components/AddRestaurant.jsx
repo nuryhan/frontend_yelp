28f4bd5f694690399532be6235567b9278397f05
nuryhan <nurmuhammet.muradov0309@gmail.com>
2021-10-20 20:35:02 +0500

    create a restaurant

--- src/components/AddRestaurant.jsx
+++ src/components/AddRestaurant.jsx
@@ -1,21 +1,44 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import RestaurantFinder from "../apis/RestaurantFinder";
+import { RestaurantsContext } from '../contextAPI/RestaurantsContext';
 
 const AddRestaurant = () => {
+    
+    const {addRestaurants} = useContext(RestaurantsContext);
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [priceRange, setPriceRange] = useState("Price Range");
+
+    const handleSubmit = async (e)=> {
+        e.preventDefault()
+        try {
+            const response = await RestaurantFinder.post("/", {
+                 name,
+                 location,
+                price_range: priceRange,
+            } );
+            addRestaurants(response.data.data.restaurant);
+            console.log(response);
+        }catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
             <div className = "container  ">
             <form  >
                 <div className="row justify-content-between" >
                 
                     <div className="col-3">
-                        <input type="text" className="form-control" placeholder="name"/>
+                        <input value={name} onChange={e=> setName(e.target.value)} type="text" className="form-control" placeholder="name"/>
                     </div>
                     <div className="col-3">
-                        <input type="text" className="form-control" placeholder="location" />
+                        <input value={location} onChange={e=> setLocation(e.target.value)} type="text" className="form-control" placeholder="location" />
                     </div>
                     <div className="col-3">
                     
-                        <select className= "form-control" >
-                            <option value="selected"  disabled>Price Range</option>
+                        <select value={priceRange} onChange={e => setPriceRange(e.target.value)} className= "form-control" >
+                            <option disabled>Price Range</option>
                             <option value="1">$</option>
                             <option value="2">$$</option>
                             <option value="3">$$$</option>
@@ -24,7 +47,7 @@ const AddRestaurant = () => {
                         </select>
                     </div>
                     <div className="col d-flex justify-content-center">
-                    <button className="btn btn-primary" > Add Restaurant</button>
+                    <button onClick={handleSubmit}  type="submit" className="btn btn-primary" > Add Restaurant</button>
                     </div>
                     
                    
